3b6a6c46ddd9d6fca312584a2b585a245252d21b
Taylor Behrens <126136711+taylorbeee@users.noreply.github.com>
2023-05-11 14:22:18 -0700

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,16 @@
+var available = document.querySelector("h3");
+console.log(available);
+var mainTitle = document.querySelector(".main-title");
+console.log(mainTitle);
+
+var firstItem = document.querySelector("ul li");
+console.log(firstItem);
+
+var intro = document.querySelector(".intro p");
+intro.style.color = "purple";
+console.log(intro);
+
+var firstImg = document.querySelector("img");
+firstImg.src = "img/lulu.jpeg";
+firstImg.alt = "Lulu bouquet";
+console.log(firstImg);
